f0dc1c3942fe848c7408ae4fa587143da63e9db2
notaarryan <aryanparmar2503@gmail.com>
2024-10-17 00:15:44 -0400

    added more functions with explaination

--- main.py
+++ main.py
@@ -14,3 +14,17 @@ print(a.shape,end="\n\n") #prints the number of 2D,3D... arrays
 print(a.size, end="\n\n") #prints the number of elements in the array
 print(b.nbytes,end="\n\n") #prints the number of bytes this array takes
 print(b.dtype,end="\n\n") #prints the data type of the elements
+
+#np.arange(start,stop,step)
+a = np.arange(10,100) #creates a np array in the range[10,100) can have step value too
+print(a,end="\n\n")
+
+#np.random.permutatuon(iterable) aranges the elements of the iterable in a random order and returns a np array of it
+a = np.random.permutation((1,2,3,4,5,6))
+print(a,end="\n\n")
+
+#returns a random interger in the provided range
+# np.random.randint(low,high,size) if size is not specified it returns a number
+#if size is specified it returns a np array
+a = np.random.randint(0,20)
+print(a,end="\n\n")
